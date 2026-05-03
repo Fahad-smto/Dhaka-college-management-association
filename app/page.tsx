5e668f744f887026a65fa932bbba7712ccfe55d3
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-04 01:59:57 +0600

    update welcome message in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        Dcma code is here to stay! We are committed to providing a safe and secure environment for all users. We will continue to enforce our policies and take action against any content that violates our guidelines. Thank you for being a part of our community and helping us maintain a positive experience for everyone.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
