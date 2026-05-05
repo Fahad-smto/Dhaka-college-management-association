e7438af8e0d457fa688c15a1409c8d0813b00cd0
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-05 23:59:11 +0600

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/NavBar";
 import Banner from "./components/Banner";
 import FeaturedEvents from "./components/FeaturedEvents";
 import LeadershipTeam from "./components/LeadershipTeam";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
      <Banner />
      <FeaturedEvents></FeaturedEvents>
      <LeadershipTeam></LeadershipTeam>
+     <Footer></Footer>
     </div>
   );
 }
