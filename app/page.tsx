e27d59aec6046e347c2abdfeff78a0477ae36329
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-05 22:44:41 +0600

    featured events added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/NavBar";
 import Banner from "./components/Banner";
+import FeaturedEvents from "./components/FeaturedEvents";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
      <Banner />
-     
+     <FeaturedEvents></FeaturedEvents>
     </div>
   );
 }
